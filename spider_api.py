78b646d518fd24e7fddceef7a349e4700386bf6a
Craynic <cyc19921205@gmail.com>
2014-02-12 17:01:22 +0800

    same as last

--- spider_api.py
+++ spider_api.py
@@ -9,27 +9,10 @@ import random
 import pymongo
 import fake_useragent
 
-def init():
-	print 'Reading configurations...',
-	config = ConfigParser.ConfigParser()
-	config.read(["spider.cfg", "spider_default.cfg"])
-	global _domain, _language, _realm, _guild, _protocol, _max_level, _output, _ua
-	_domain = config.get('default', 'domain')
-	_language = config.get('default', 'language')
-	_realm = config.get('default', 'realm')
-	_guild = config.get('default', 'guild')
-	_protocol = config.get('default', 'protocol') + r'://'
-	_max_level = config.getint('default', 'max_level')
-	_output = False
-	print 'Done.' 
-	print 'Preparing fake_useragent...',
-	_ua = fake_useragent.UserAgent()
-	print 'Done.'
-	
 def get_json(url):
-	if not _output:
-		print 'Fetching:', url
+	print 'Fetching:', url
 	while True:
+		time.sleep(1)
 		try:
 			request = urllib2.Request(url)
 			page = urllib2.urlopen(request)
@@ -54,50 +37,75 @@ def get_json(url):
 				else:
 					print 'Unkown error.',
 		print 'Try again.'
-		time.sleep(random.random()*5+1)
-	
-def main():
-	print 'Connect to mongodb...',
-	try:
-		mongodb = pymongo.MongoClient()
-		dbc = mongodb['wow']['lastcampaign']
-	except:
-		print 'Failed.'
-		print 'Output to stdout.'
-		_output = True
-	else:
+		time.sleep(random.random()*5)
+			
+class spider:
+	def __init__(self):
+		print 'Reading configurations...',
+		config = ConfigParser.ConfigParser()
+		config.read(["spider.default.cfg", "spider.cfg"])
+		self.config = config
+		print 'Done.' 
+		print 'Preparing fake_useragent...',
+		_ua = fake_useragent.UserAgent()
 		print 'Done.'
-	realm = _realm.replace(' ', '%20')
-	guild = _guild.replace(' ', '%20')
-	guild_url = '%s%s/api/wow/guild/%s/%s?fields=members' %\
-		(_protocol, _domain, realm, guild)
-	gj = get_json(guild_url)
-	for person in gj['members']:
-		i = dbc.find({'name':person['character']['name'].encode('utf-8')})
-		if i.count() > 0:
-			continue
-		info = {}
-		character = person['character']
-		info['name'] = character['name'].encode('utf-8')
-		info['race'] = character['race']
-		info['class'] = character['class']
-		info['gender'] = character['gender']
-		info['level'] = character['level']
-		info['realm'] = character['guildRealm'].encode('utf-8')
-		info['guild'] = character['guild'].encode('utf-8')
-		if info['level'] >= _max_level:
-			chara_url = '%s%s/api/wow/character/%s/%s?fields=items,professions,pvp' %\
-				(_protocol, _domain, realm, info['name'])
-			cj = get_json(chara_url)
-			if cj != None:
-				info['item_level'] = cj['items']['averageItemLevel']
-				prof = []
-				for i in cj['professions']['primary']:
-					prof.append(i['name'].encode('utf-8'))
-				info['profession'] = prof
-				info['personal_rating'] = cj['pvp']["brackets"]["ARENA_BRACKET_RBG"]["rating"]
-		dbc.insert(info)
+		
+	def spide(self):
+		
+		c = self.config
+		
+		print 'Connecting to mongodb at %s on %s ...' % (c.get('mongodb', 'domain'), c.getint('mongodb', 'port')),
+		try:
+			mongodb = pymongo.MongoClient(c.get('mongodb', 'domain'), c.getint('mongodb', 'port'))
+		except Exception as e:
+			print 'Failed.'
+			print e
+			return
+		else:
+			print 'Done.'
+		print 'Using db %s, collection %s....' % (c.get('mongodb', 'db'), c.get('mongodb', 'collection')),
+		try:
+			dbc = mongodb[c.get('mongodb', 'db')][c.get('mongodb', 'collection')]
+		except Exception as e:
+			print 'Failed.'
+			print e
+			return
+		else:
+			print 'Done.'
+			
+		domain = c.get('source', 'domain')
+		realm = c.get('source', 'realm').replace(' ', '%20')
+		guild = c.get('source', 'guild').replace(' ', '%20')
+		protocol = c.get('source', 'protocol') + '://'
+		guild_url = '%s%s/api/wow/guild/%s/%s?fields=members' %\
+			(protocol, domain, realm, guild)
+		gj = get_json(guild_url)
+		for person in gj['members']:
+			i = dbc.find({'name':person['character']['name'].encode('utf-8')})
+			if i.count() > 0:
+				continue
+			info = {}
+			character = person['character']
+			info['name'] = character['name'].encode('utf-8')
+			info['race'] = character['race']
+			info['class'] = character['class']
+			info['gender'] = character['gender']
+			info['level'] = character['level']
+			info['realm'] = character['guildRealm'].encode('utf-8')
+			info['guild'] = character['guild'].encode('utf-8')
+			if info['level'] >= c.getint('default', 'max_level'):
+				chara_url = '%s%s/api/wow/character/%s/%s?fields=items,professions,pvp' %\
+					(protocol, domain, realm, info['name'])
+				cj = get_json(chara_url)
+				if cj != None:
+					info['item_level'] = cj['items']['averageItemLevel']
+					prof = []
+					for i in cj['professions']['primary']:
+						prof.append(i['name'].encode('utf-8'))
+					info['profession'] = prof
+					info['personal_rating'] = cj['pvp']["brackets"]["ARENA_BRACKET_RBG"]["rating"]
+			dbc.insert(info)
 
 if __name__ == '__main__':
-	init()
-	main()
+	s = spider()
+	s.spide()
